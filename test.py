b1c3b0c4bb4e5959580896598389f612b085cdd1
wangfeili <37933241+wangfeili@users.noreply.github.com>
2018-12-19 23:08:02 +0800

    test 11
    12.19 作用域

--- test.py
+++ test.py
@@ -44,11 +44,93 @@
 # '''
 # print(a)
 
+# '''
+# 重构3
+# '''
+# from random import randint as ri
+# print([n for n in [ri(1,99) for i in range(9)] if n % 2])
+
+# a = map((lambda x: x + 2),[1,2,3,4,5])
+#
+# print(list(a))
+from operator import add
+from functools import reduce,partial
+
+# '''
+# 为啥在python中不自带reduce？？？
+# '''
+# a = reduce((lambda x,y: x+y),range(5))
+# print(a)
+
+# addl = partial(add,1)
+# print(addl(1))
+
+#
+# def foo():
+#     m = 3
+#     def bar():
+#         n =4
+#         print(n+m)
+# print(m)
+# bar()
+
 '''
-重构3
+closuer
 '''
-from random import randint as ri
-print([n for n in [ri(1,99) for i in range(9)] if n % 2])
+# def counter(s=0):
+#     count = [s]
+#     def incr():
+#         count[0] += 1
+#         return count[0]
+#     return incr()
+#
+# count = counter(5)
+# print(count)
+# print(count())
 
 
+'''
+lambda和作用域
+'''
+# x = 10
+# def foo():
+#     y = 5
+#     bar = lambda :x+y
+#     print(bar())
+#
+# foo()
 
+#
+# x = 10
+# def foo():
+#     y = 5
+#     bar = lambda y=y:x+y
+#     print(bar())
+#     y = 10
+#     print(bar())
+#
+# foo()
+
+'''
+test
+'''
+j, k = 1, 2
+
+def proc1():
+    j, k = 3, 4
+    print("j == %d and k == %d" % (j, k))
+    k = 5
+
+
+def proc2():
+    j = 6
+    proc1()
+    print("j == %d and k == %d" % (j, k))
+
+k = 7
+proc1()
+print("j == %d and k == %d" % (j, k))
+
+j = 8
+proc2()
+print("j == %d and k == %d" % (j, k))
