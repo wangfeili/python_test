64a69646e343f6806771ad2125bbf181e1b323c8
wangfeili <37933241+wangfeili@users.noreply.github.com>
2019-04-21 20:10:35 +0800

    test
    test

--- threading_test.py
+++ threading_test.py
@@ -1,20 +1,31 @@
 import threading
 import time
 
+class MyThread(threading.Thread):
+    def __init__(self,num):
+        threading.Thread.__init__(self)
+        self.num = num
+
+    def run(self):
+        print("running on number :%s" % self.num)
+
+        time.sleep(3)
 
 def Hi(num):
     print('hello %s %s ' % (num, time.time()))
-    time.sleep(3)
+    time.sleep(int(num))
     print('end %s %s ' % (num, time.time()))
 
 if __name__ == '__main__':
 
-    t1 = threading.Thread(target=Hi, args=(10,)) #只是创建了一个线程对象
+    t1 = threading.Thread(target=Hi, args=(3,)) #只是创建了一个线程对象
+    t1.setDaemon(True)
     t1.start()
-    t1.join()
+    # t1.join()
 
-    t2 = threading.Thread(target=Hi, args=(9,))
+    t2 = threading.Thread(target=Hi, args=(5,))
+    #t2.setDaemon(True)
     t2.start()
-    t2.join()
+    # t2.join()
 
     print("ending.....")
